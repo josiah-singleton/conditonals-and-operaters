4677dbe4a073e7809dcd79592560a77a7841d16d
josiah-singleton <91489627+josiah-singleton@users.noreply.github.com>
2021-11-30 09:18:22 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let x;
+let y;
+x = parseInt(prompt("choose a number between 1-10"));
+console.log (x)
+if (x<=5) {
+    alert("your number is less than 6")
+} else {
+    alert("your number is greater han 5")
+}
+y = parseInt(prompt("choose a number between 1-10"))
+console.log (y)
+if (y % 2 == 0) {
+    alert("your number is even")
+} else {
+    alert("your number is odd")
+}
